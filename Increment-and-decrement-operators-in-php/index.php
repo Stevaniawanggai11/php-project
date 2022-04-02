443ff8917ad595f9e2f0436f8107781109af6fb6
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-02 11:25:55 +0900

    Create index.php

---
+++ Increment-and-decrement-operators-in-php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+    
+<?php
+
+    //Increment/Decrement Operators
+    $x = 10;
+    echo $x--;
+    echo $x;
+
+?>
+
+</body>
+
+</html>
