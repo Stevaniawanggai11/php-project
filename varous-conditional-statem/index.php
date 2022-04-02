5d0f80dfd3afe12c9ea1320969cd981eabd0bd34
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-02 11:44:50 +0900

    Create index.php

---
+++ varous-conditional-statem/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+    
+<?php
+
+    $x = 2;
+
+    if ($x == 1) {
+    echo "Daniel is very handsome!";
+    } 
+    elseif ($x == 2) {
+        echo "Daniel is kinda handsome!";
+    }
+    elseif ($x == 3) {
+        echo "Daniel is kinda handsome!";
+    }
+    elseif ($x == 4) {
+        echo "Daniel is kinda handsome!";
+    }
+        else {
+    echo "Daniel is very ugly!";
+    }
+
+?>
+
+</body>
+
+</html>
