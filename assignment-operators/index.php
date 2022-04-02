b55285303a5307af4f0123f55f7b81686b6cae14
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-02 11:19:13 +0900

    new

---
+++ assignment-operators/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+    
+<?php
+    //Assignment Operators
+    $x = 100;
+    $x /= 20;
+
+
+
+    echo $x
+
+?>
+
+</body>
+
+</html>
---
+++ comparison-operators-in-php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+    
+<?php
+
+    // Comparison Operators
+    $x = 10;
+    $y = 10;
+
+    if ($x !== $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }
+
+?>
+</body>
