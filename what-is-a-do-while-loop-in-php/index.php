465274498b23b47b0fec6068f4e4be80d978257e
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 11:24:59 +0900

    Create index.php

---
+++ what-is-a-do-while-loop-in-php/index.php
@@ -0,0 +1,28 @@
+!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+   
+
+<?php
+    // Loop
+
+    // While Loop
+    // Do while Loop
+    // For Loop
+    // Foreach Loop
+    $x = 1;
+    while ($x < 5) {
+        echo "hi there<br>";
+        $x++;
+    }
+    while ($x <= 5);
+?>
+
+</body>
+
+</html>
