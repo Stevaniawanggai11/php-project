4732ff2eb4994bb8929edc5c9dc0709b9bd87003
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 10:21:00 +0900

    Create index.php
    new

---
+++ switch-statements-in-php/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+    
+<?php
+
+    $x = 8;
+
+    switch ($x) {s
+        case 1:
+            echo " The answer is 1";
+        break;
+        case 2:
+            echo "The answer is 2";
+        break;
+        case 3:
+            echo "The answer is 3";
+        break;
+        case 4:
+            echo "The answer is 4";
+        break;
+        default:
+            echo "There is no answer";
+        
+    }
+
+?>
+
+</body>
+
+</html>
+
