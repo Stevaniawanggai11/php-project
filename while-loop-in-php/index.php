f9be6bb652e339b31b42a2d7eac7143ad8d7d0f4
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 11:20:52 +0900

    Create index.php

---
+++ while-loop-in-php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+   
+
+<?php
+    // Loop
+
+    // While Loop
+    // Do while Loop
+    // For Loop
+    // Foreach Loop
+    $x = 1;
+    while ($x < 5) {
+        echo "hi there<br>";
+        $x++;
+    }
+?>
+
+</body>
+
+</html>
