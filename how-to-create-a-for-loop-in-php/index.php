af88360e08302836bd252b774fbe275e6cf7060b
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 11:30:41 +0900

    Create index.php

---
+++ how-to-create-a-for-loop-in-php/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+   
+
+<?php
+    // Loop
+
+    // While Loop
+    // Do while Loop
+    // For Loop
+    // Foreach Loop
+
+    for ($x = 1; $x <= 10; $x++) {
+        echo "hi<br>";
+    }
+?>
+
+</body>
+
+</html>
