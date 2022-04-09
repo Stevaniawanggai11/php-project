8e6598ffe89baa5eec268fa67680d036a6dd2aef
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 11:48:02 +0900

    Create index.php

---
+++ using-arrays-in-php-to-store-data/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+
+</head>
+
+<body>
+    
+<?php
+
+    $array = array("Daniel", "Jane", "Jacob", "John", "Mariane");
+
+    $data1 = "Daniel";
+    $data1 = "Jane";
+    $data1 = "Jacob";
+    $data1 = "John";
+
+?>
+
+</body>
+
+</html>
