b024818e124c04721cba8f2f6691a172ac78fd59
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 11:12:28 +0900

    new
    new

---
+++ scheduled-message-programming/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<?php
+    $dayofweek = date("w");
+   
+    switch ($dayofweek) {
+        case 1:
+            echo "It is Monday!";
+            break;
+        case 2:
+             echo "It is Tuesday!";
+            break;
+        case 3:
+            echo "It is Wednesday!";
+             break;
+        case 4:
+            echo "It is Thursday!";
+            break;
+        case 5:
+             echo "It is Friday!";
+            break;
+        case 6:
+            echo "<p>It is saturday!</p>";
+            break;
+        case 7:
+            echo "It is Sunday!";
+            break;
+            
+    }
+?>
+
+</body>
+
+</html>
