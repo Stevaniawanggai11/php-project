4c2bab32d84cff10deffef16d508b7b323eae1b5
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-09 11:38:41 +0900

    Create index.php

---
+++ how-to-create-a-foreach-loop-in-php/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+   
+
+<?php
+    // Loop
+
+    // While Loop
+    // Do while Loop
+    // For Loop
+    // Foreach Loop
+    $array = array("Daniel", "Jane", "Jacob");
+
+    foreach ($array as $loopdata) {
+        echo "My name is ".$loopdata."<br>";
+    }
+
+?>
+
+</body>
+
+</html>
