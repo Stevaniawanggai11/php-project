31f6a92c4722cbfb16059348b6396d6b71a9a6af
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-23 10:52:06 +0900

    Create index.php

---
+++ post-and-get-superglobals-in-php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>   
+<?php
+/*
+$GLOBALS
+$_POST
+$_GET
+$_COOKIE
+$_SESSION
+*/
+
+?>
+
+<form method="GET">
+    <input type="hidden" name="name" value="value">
+    <button type="submit">PRESS ME!</button>
+</form>
+
+</body>
+
+</html>
