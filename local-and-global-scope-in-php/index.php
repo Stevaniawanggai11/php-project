e811cc08e982e7d6671b8e6caf9fe842cd334c9c
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-23 10:28:07 +0900

    Create index.php

---
+++ local-and-global-scope-in-php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+</head>
+
+<body>
+    
+<?php
+*/
+
+$x =5;
+
+function something() {
+    $y = 10;
+}
+
+echo $y;
+?>
+
+</body>
+
+</html>
