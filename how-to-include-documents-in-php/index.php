4ec05eb4508984344c0ec05731f3c91d1c686f0f
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-04-23 10:19:23 +0900

    new

---
+++ how-to-include-documents-in-php/index.php
@@ -0,0 +1,33 @@
+<?php
+    include_once 'functions/user-functions.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Title of the document</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="">nav1</a></li>
+            <li><a href="">nav2</a></li>
+            <li><a href="">nav3</a></li>
+            <li><a href="">nav4</a></li>
+        </ul>
+    </nav>
+</header>
+
+<section>
+    <div class="someMainContent">
+        <h1>Hi there!</h1>
+    </div>
+</section>
+
+</body>
+
+</html>
