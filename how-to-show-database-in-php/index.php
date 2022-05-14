bed0d057a4fc7d3f8ef8f3b009f29b28944dec7e
Stevania Mauren Wanggai <91767848+Stevaniawanggai11@users.noreply.github.com>
2022-05-14 09:51:19 +0900

    Update index.php

--- how-to-show-database-in-php/index.php
+++ how-to-show-database-in-php/index.php
@@ -8,6 +8,20 @@
     <title></title>
 </head>
 <body>
+
+<?php
+    $sql = "SELECT * FROM users;";
+    $result = mysqli_query($conn,$sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)){
+            echo $row['user_uid'] . "<br>";
+            
+        }
+    }
+?>  
+
     
 </body>
 </html>
